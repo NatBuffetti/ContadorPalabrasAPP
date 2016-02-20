20036fda4e1f69a24511517ee5dfebf08184223c
Nat <natalia.buffetti@gmail.com>
2016-02-20 13:33:24 -0300

    contadorPalabras - word counter

--- contadorPalabras.js
+++ contadorPalabras.js
@@ -1,6 +1,43 @@
 /**
  * Created by Nat on 13/2/2016.
  */
+var WordCounterAPP = {
+cantidadPalabras:0,
+    contar:function(){
+        var text = document.getElementById("text").value;
+        var wordMap = this.doMagic(text);
+        console.log(wordMap);
+        for (var key in wordMap){
+        var item = $('<li class = "list-group-item">'+key+":"+wordMap[key]+'</li>');
+        $("#words").append(item);
+        }
+    }
+    ,doMagic:function(text){
+        var palabrasCant = {};
+        var fraseA = text.split(" ");
+        console.log(fraseA);
+        for (var i = 0; i < fraseA.length; i++) {
+            if (palabrasCant[fraseA[i]]) {
+                palabrasCant[fraseA[i]]++;
+            } else {
+                palabrasCant[fraseA[i]] = 1;
+            }
+        }
+        return palabrasCant;
+    }
+};
+window.APP = WordCoutnerAPP;
+
+
+
+/*        this.mostrar(text);
+    },
+    mostrar:function(text){
+        console.log(text);
+    }
+};
+
+window.APP = WordCounterAPP;
 
 function contarPalabras (frase) {
     var palabrasCant = {};
@@ -15,4 +52,4 @@ function contarPalabras (frase) {
     }
     console.log(palabrasCant);
 }
-contarPalabras("club atletico river plate river river plate");
+contarPalabras(text);*/
